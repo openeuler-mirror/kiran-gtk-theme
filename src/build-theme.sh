ae76a52279c54af1ae9db351e35b82d18eeda7d3
tangjie02 <tangjie02@kylinsec.com.cn>
2021-10-13 14:28:06 +0800

    feature(scss): Add colors tables and update scss combined with adwaita theme
    - 添加颜色表，并结合adwaita主题进行调整
    
    Signed-off-by: tangjie02 <tangjie02@kylinsec.com.cn>

--- src/build-theme.sh
+++ src/build-theme.sh
@@ -1,57 +1,69 @@
+#!/bin/sh
+set -e
+
+# Usage: create_folders <target-directory>
+create_folders () {
+    for j in gtk-2.0 gtk-3.0; do
+        if ! [ -d "$1/$j" ]; then
+            mkdir -p "$1/$j"
+        fi
+    done
+}
+
+# Usage render_theme <colorscheme> <theme-name> <theme-dir> <colorschemebase>
+render_theme () {
+    create_folders "$3"
+
+    # index.theme
+    sed -e "s/META_THEME_NAME/$2/g" -e "s/GTK_THEME_NAME/$2/g" index.theme > $3/index.theme
+
+    # gtk2
+    cp -R gtk2/* "$3/gtk-2.0/"
+
+    # gtk3
+    python3 render_assets.py -c "$1" -a "$3/assets" \
+      -g "$3/gtk-2.0" -G "$3" -b "$4"
+
+    sassc -I "$3" gtk3/gtk.scss "$3/gtk-3.0/gtk.css" 
+    rm -f "$3/_global.scss"
+
+    # assets
+    cp -r assets/ "$3"
+}
 
 
+COLOR_SCHEME=""
 THEME_NAME=""
-COLOR_FILE=""
 THEME_BUILD_DIR=""
 
 while [ "$#" -gt 0 ]; do
-  case "$1" in
-    -h|--help)
-      echo "$0: build Kiran theme"
-      echo "Usage: $0 [-t THEME_NAME] [-c COLOR_FILE] [-d DEST_DIRECTORY]"
-      echo
-      echo "Arguments:"
-      echo "  -h, --help                         show this help"
-      echo "  -t, --theme-name THEME_NAME        theme name"
-      echo "  -c, --color-file COLOR_FILE        base color file"
-      echo "  -d, --dest-dir   DEST_DIRECTORY    the directory to install the theme"
-      exit 0
-    ;;
-    -t|--theme-name)
-      shift
-      THEME_NAME="$1"
-    ;;
-    -c|--color-file)
-      shift
-      COLOR_FILE="$1"
-    ;;
-    -d|--dest-dir)
-      shift
-      THEME_BUILD_DIR="$1"
-    ;;
-  esac
-  shift
+    case "$1" in
+        -h|--help)
+            echo "$0: build Kiran theme"
+            echo "Usage: $0 [-t THEME_NAME] [-c COLOR_FILE] [-d DEST_DIRECTORY]"
+            echo
+            echo "Arguments:"
+            echo "  -h, --help                           show this help"
+            echo "  -c, --color-scheme COLOR_SCHEME      use color scheme with name COLOR_SCHEME"
+            echo "  -t, --theme-name   THEME_NAME        theme name"
+            echo "  -d, --dest-dir     DEST_DIRECTORY    the directory to install the theme"
+            exit 0
+        ;;
+        -c|--color-scheme)
+            shift
+            COLOR_SCHEME="$1"
+        ;;
+        -t|--theme-name)
+            shift
+            THEME_NAME="$1"
+        ;;
+        -d|--dest-dir)
+            shift
+            THEME_BUILD_DIR="$1"
+        ;;
+    esac
+    shift
 done
 
 
-if [ -z "${THEME_NAME}" ]; then
-    THEME_NAME="KiranNew"
-fi
-
-
-mkdir -p ${THEME_BUILD_DIR}
-sed -e "s/META_THEME_NAME/${THEME_NAME}/g" -e "s/GTK_THEME_NAME/${THEME_NAME}/g" index.theme > ${THEME_BUILD_DIR}/index.theme
-
-# ---------------------------------------------------------  gtk2 ---------------------------------------------------------
-THEME_GTK2_BUILD_DIR=${THEME_BUILD_DIR}/gtk-2.0
-mkdir -p ${THEME_GTK2_BUILD_DIR}
-cp gtk2/gtkrc ${THEME_GTK2_BUILD_DIR}
-
-
-# ---------------------------------------------------------  gtk3 ---------------------------------------------------------
-THEME_GTK3_BUILD_DIR=${THEME_BUILD_DIR}/gtk-3.0
-mkdir -p ${THEME_GTK3_BUILD_DIR}
-cp ${COLOR_FILE} ${THEME_GTK3_BUILD_DIR}/_base-colors.scss
-cp -r assets ${THEME_GTK3_BUILD_DIR}/
-sassc -I ${THEME_GTK3_BUILD_DIR} gtk3/gtk.scss ${THEME_GTK3_BUILD_DIR}/gtk.css
-rm ${THEME_GTK3_BUILD_DIR}/_base-colors.scss
+render_theme "./colors/${COLOR_SCHEME}.colors" "${THEME_NAME}" "${THEME_BUILD_DIR}" "./colors/base.colors"
